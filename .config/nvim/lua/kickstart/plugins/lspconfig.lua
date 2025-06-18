a9022ad83d88165caa59035038408c67f23f47c0
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-06-18 01:10:37 +0100

    add python & java lsp

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -210,8 +210,9 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
         -- rust_analyzer = {},
+        pyright = {},
+        jdtls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
