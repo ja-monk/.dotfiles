4023949085954fb3f2a4f6ee2cf1e19a6debe20b
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-06-21 20:22:02 +0100

    add bash lsp

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -198,9 +198,8 @@ return {
             --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
             local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-            -- Enable the following language servers
-            --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-            --
+            -- NOTE: Enable the following language servers
+
             --  Add any additional override configuration in the following tables. Available keys are:
             --  - cmd (table): Override the default command used to start the server
             --  - filetypes (table): Override the default list of associated filetypes for the server
@@ -208,19 +207,14 @@ return {
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
+                -- NOTE: See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 -- gopls = {},
-                -- rust_analyzer = {},
                 pyright = {},
                 jdtls = {},
-                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                --
+                bashls = {},
+
                 -- Some languages (like typescript) have entire language plugins that can be useful:
                 --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`ts_ls`) will work just fine
-                -- ts_ls = {},
-                --
 
                 lua_ls = {
                     -- cmd = { ... },
