cf3aecc2aa3612ab0f8bff4c5c154682a96cf0a9
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-06-22 02:04:42 +0100

    enable signs in side bar for todo-comments plugin

--- .config/nvim/lua/kickstart/plugins/todo-comments.lua
+++ .config/nvim/lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,20 @@
 -- Highlight todo, notes, etc in comments
 return {
-    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+    {
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {
+            signs = true,
+            -- Define colours seperate to theme
+            --colors = {
+            --    error = { "#F7768E" },
+            --    warning = { "#FAB387" },
+            --    info = { "#7FC1FA", "bold", "underline"},
+            --    hint = { "#9ECE6A" },
+            --    default = { "#B4BEFE" },
+            --    test = { "#DDB6F2" },
+            --}
+        },
+    },
 }
--- vim: ts=2 sts=2 sw=2 et
