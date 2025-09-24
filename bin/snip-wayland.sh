62ccd4741fd191371aa2ca00d2f588bdbb43d427
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-09-24 20:43:35 +0100

    add wayland snip script with --edit option

---
+++ bin/snip-wayland.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+timestamp=$(date +'%Y-%m-%d_%H-%M-%S')
+output_dir="$HOME/Pictures/screenshots"
+edit=false
+
+# check for edit argument
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -e|--edit)
+            edit=true
+            shift
+        ;;
+        *)
+            notify-send "Unrecognised argument to screenshot: $1"
+            exit 1
+        ;;
+    esac
+done
+
+# check for screenshot dir & attempt to create if cant find
+[[ -d "$output_dir" ]] || {
+    mkdir -p "$output_dir"
+ 
+    if [[ ! -d "$output_dir" ]]; then
+      notify-send \
+          "Screenshot directory does not exist and creation failed: $output_dir" \
+          -u critical \
+          -t 3000
+      exit 1
+    fi
+}
+
+if [[ "$edit" == true ]]; then
+    pkill slurp || hyprshot -m ${1:-region} --raw |
+      satty --filename - \
+        --output-filename "$output_dir/screenshot-${timestamp}.png" \
+        --early-exit \
+        --actions-on-enter save-to-clipboard \
+        --copy-command 'wl-copy'
+else
+    pkill slurp || hyprshot -m ${1:-region} --raw | wl-copy
+fi
+
