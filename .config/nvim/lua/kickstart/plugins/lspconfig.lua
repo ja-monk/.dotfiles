765b6939b44cc9d139deb19c85925691d6db784c
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-12-05 19:41:59 +0000

    add go lsp into mason lsp config

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -208,12 +208,16 @@ return {
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
                 -- NOTE: See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                -- gopls = {},
+
+                gopls = {},
+
                 pyright = {
                     venvPath = ".",
                     venv = ".venv",
                 },
+
                 --jdtls = {},   -- requires java 21
+
                 bashls = {},
 
                 -- Some languages (like typescript) have entire language plugins that can be useful:
